99ec5861e826f15e98e9fa2255e424013bf83a98
AZIZ ULLAH TAREK <azizullaht2002@gmail.com>
2025-10-21 23:34:00 +0600

    All set up done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="text-6xl mx-auto">
+      <h1 >Vite + React</h1> 
+      </div>
+      
+     
+    </>
+  )
+}
+
+export default App
